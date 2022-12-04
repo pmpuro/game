9c115670b87e00022100f76524c51a9c93e9e678
Panu Puro <panu.puro@gmail.com>
2022-12-04 14:24:54 +0200

    testing fn needs to be inside the call of deftest fn to actually fail.

--- test/game/core_test.clj
+++ test/game/core_test.clj
@@ -29,23 +29,29 @@
   (t/is (= [[1 0] [2 0]] (i/collect-to-direction :x [0 0] [1 0] { [0 0] :x, [1 0] :x, [2 0] :x })))
   )
 
-(t/testing "value-of"
-  (t/testing "x already in the pos"
-    (t/is (= 0 (i/value-of :x [0 0] { [0 0] :x})))
-    (t/is (= 0 (i/value-of :x [0 0] { [0 0] :x, [0 1] :x})))
-    (t/is (= 0 (i/value-of :x [0 0] { [0 0] :x, [1 0] :x}))))
-  (t/testing "other piece already in the pos"
-    (t/is (= 0 (i/value-of :x [0 0] { [0 0] :y}))))
-  (t/testing "empty board"
-    (t/is (= 0 (i/value-of :x [0 0] {}))))
-  (t/testing "y"
-    (t/is (= 2 (i/value-of :x [1 1] { [1 0] :x, [1 2] :x})))
-    (t/is (= 2 (i/value-of :x [1 1] { [1 2] :x, [1 3] :x})))
-    (t/is (= 2 (i/value-of :x [1 2] { [1 0] :x, [1 1] :x}))))
-  (t/testing "x"
-    (t/is (= 2 (i/value-of :x [1 1] { [0 1] :x, [2 1] :x})))
-    (t/is (= 2 (i/value-of :x [1 1] { [2 1] :x, [3 1] :x})))
-    (t/is (= 2 (i/value-of :x [2 1] { [0 1] :x, [1 1] :x}))))
+(t/deftest test-value-of
+  (t/testing "value-of"
+    (t/testing "x already in the pos"
+      (t/is (= 0 (i/value-of :x [0 0] { [0 0] :x})))
+      (t/is (= 0 (i/value-of :x [0 0] { [0 0] :x, [0 1] :x})))
+      (t/is (= 0 (i/value-of :x [0 0] { [0 0] :x, [1 0] :x}))))
+    (t/testing "other piece already in the pos"
+      (t/is (= 0 (i/value-of :x [0 0] { [0 0] :y}))))
+    (t/testing "empty board"
+      (t/is (= 0 (i/value-of :x [0 0] {}))))
+    (t/testing "y"
+      (t/is (= 2 (i/value-of :x [1 1] { [1 0] :x, [1 2] :x})))
+      (t/is (= 2 (i/value-of :x [1 1] { [1 2] :x, [1 3] :x})))
+      (t/is (= 2 (i/value-of :x [1 2] { [1 0] :x, [1 1] :x}))))
+    (t/testing "x"
+      (t/is (= 2 (i/value-of :x [1 1] { [0 1] :x, [2 1] :x})))
+      (t/is (= 2 (i/value-of :x [1 1] { [2 1] :x, [3 1] :x})))
+      (t/is (= 2 (i/value-of :x [2 1] { [0 1] :x, [1 1] :x}))))
+    (t/testing "diagonal down"
+      (t/is (= 2 (i/value-of :x [1 1] { [0 0] :x, [2 2] :x}))))
+    (t/testing "diagonal up"
+      (t/is (= 2 (i/value-of :x [1 1] { [0 2] :x, [2 0] :x}))))
+    )
   )
 
 (t/deftest test-valid-test
